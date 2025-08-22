bd7ef4d16468deadeb623003394267f023cf1ec8
unknown <ksiaczeq@op.pl>
2025-08-22 12:24:27 +0000

    fixed user

--- src/main/java/com/neueda/portfoliomanager/controllers/StockController.java
+++ src/main/java/com/neueda/portfoliomanager/controllers/StockController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/stocks")
+@RequestMapping("/stocks")
 public class StockController {
 
     private final StockRepository stockRepository;
--- src/main/java/com/neueda/portfoliomanager/entity/User.java
+++ src/main/java/com/neueda/portfoliomanager/entity/User.java
@@ -11,7 +11,6 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-
 @Table(name = "users")
 public class User {
 
