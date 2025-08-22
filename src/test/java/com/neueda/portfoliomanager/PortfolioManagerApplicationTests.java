5f22a61c43ee384f52dd2f9dc755dca8d7792dca
unknown <ksiaczeq@op.pl>
2025-08-22 08:45:52 +0000

    Created Management_Portfolio project

---
+++ src/test/java/com/neueda/portfoliomanager/PortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.portfoliomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
